3974da6bd213006b6e94894570344caa0585fb91
Austin Hammontree <30849233+AJHammontree@users.noreply.github.com>
2018-03-31 11:55:55 -0500

    Add files via upload

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+
+-- Seed Burgers into Database 
+INSERT INTO burgers(burger_name, devoured)
+VALUES ("Big Mac", false),
+  ("Whooper", false),
+  ("Cheeseburger", false),
+  ("Baconator", false),
+  ("Quarter Pounder", false)
