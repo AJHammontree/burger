02b76941227e226f414d70b625a67bc019c019be
Austin Hammontree <30849233+AJHammontree@users.noreply.github.com>
2018-03-31 12:01:46 -0500

    Add files via upload

--- config/connection.js
+++ config/connection.js
@@ -11,7 +11,7 @@ else{
   connection = mysql.createConnection({
     host     : 'localhost',
     user     : 'root',
-    password : 'Nightowl#3', // Add your password
+    password : '', // Add your password
     database : 'burgers_db' // Add your database
   });
 }
